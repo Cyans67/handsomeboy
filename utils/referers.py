7e7a693963cd9ebe1dd4c3c7522fe95bea4eced7
skrliu <774713125@qq.com>
2019-04-26 16:32:43 +0800

    xixi

---
+++ utils/referers.py
@@ -0,0 +1,7 @@
+referers = [
+    'https://movie.douban.com/subject/26100958/?from=showing',
+    'http://tieba.baidu.com/p/6112336993',
+    'http://tieba.baidu.com/p/6112637636',
+    'https://www.qiushibaike.com/article/121736807',
+    'https://www.bilibili.com/video/av50244571/?spm_id_from=333.334.b_686f6d655f706f70756c6172697a65.3'
+]

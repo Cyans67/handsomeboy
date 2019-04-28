22dda5dd78a03c2c87bfefe8ae805ba760a9be62
xue <1376297520@qq.com>
2019-04-28 21:16:45 +0800

    bash: reg setx

--- execute.bash
+++ execute.bash
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 python code/selenium_test.py
--- install.bash
+++ install.bash
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
 cd packages
 python-3.6.3.exe
 cd ..
-setx path "%path%;C:\Python27\;C:\Python27\Scripts" /m
+#setx path "%path%;C:\Python27\;C:\Python27\Scripts" /m
+% reg add KEY  /v path /t REG_SZ /d ";C:\Python36\;C:\Python36\Scripts"
 pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
